831a182522b3c793f67d78e4941fd14ef9312b6a
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-07-24 23:41:11 +0200

    Reformat JobDeatils.vue

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,18 +1,12 @@
 <template>
-    <h2>Job Details</h2>
-    <p>The job id is {{ id }}</p>
+  <h2>Job Details</h2>
+  <p>The job id is {{ id }}</p>
 </template>
 
 <script>
 export default {
-    props: ['id'],
-/*     data() {
-        return {
-            id: this.$route.params.id
-        }
-    } */
-}
+  props: ["id"],
+};
 </script>
 
-<style>
-</style>
+<style></style>
