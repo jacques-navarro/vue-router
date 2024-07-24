3395832c931c9f27e9fe41795636f43ffc37ffc2
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-07-24 22:34:39 +0200

    Add Jobs.vue

---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs">
+    <span>{{ job.title }} - {{ job.details }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [
+        { title: "UX Designer", id: 1, details: "blah" },
+        { title: "Web Developer", id: 2, details: "blah" },
+        { title: "Vue Developer", id: 3, details: "blah" },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
