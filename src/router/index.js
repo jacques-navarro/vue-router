399a0f0c15820058f95ea0740309449e3d0baecf
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-07-24 23:40:35 +0200

    Add NotFound (404)

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from "../views/Home.vue";
 import About from "../views/About.vue";
 import Jobs from "../views/jobs/Jobs.vue";
 import JobDetails from "../views/jobs/JobDetails.vue";
+import NotFound from "../views/NotFound.vue";
 
 const routes = [
   {
@@ -30,6 +31,11 @@ const routes = [
   {
     path: "/all-jobs",
     redirect: "/jobs",
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: "NotFound",
+    component: NotFound,
   },
 ];
 
