f0632ddb22635dfaf547624726d02a81b61945e1
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-07-25 10:18:00 +0200

    Use fetch request to load job details

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,11 +1,27 @@
 <template>
-  <h2>Job Details</h2>
-  <p>The job id is {{ id }}</p>
+  <div v-if="job">
+    <h2>{{ job.title }}</h2>
+    <p>The job id is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
 </template>
 
 <script>
 export default {
   props: ["id"],
+
+  data() {
+    return {
+      job: null,
+    };
+  },
+
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err.message));
+  },
 };
 </script>
 
