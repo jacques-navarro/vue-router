4d545769a27f6d95f191e573452b4d61ef31002e
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-07-25 09:57:17 +0200

    Use fetch request to get jobs data

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -14,6 +14,13 @@ export default {
       jobs: [],
     };
   },
+
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
+  },
 };
 </script>
 
